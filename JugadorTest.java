c9bcc289a6e644fdfbfdcf26f731862893e3d9d5
Erkus00 <91884756+Erkus00@users.noreply.github.com>
2022-02-05 14:09:25 +0100

    Aragon: Test tipoJugador añadidos. Story5

--- JugadorTest.java
+++ JugadorTest.java
@@ -20,26 +20,26 @@ class JugadorTest {
 		Jugador jugador = new Jugador();
 		String nombre= "hola2";
 		jugador.setNombreJugador(nombre);
-		assertEquals(nombre, jugador.getNombreJugador(), "Car·cter no permitido");
+		assertEquals(nombre, jugador.getNombreJugador(), "Car√°cter no permitido");
 		
 	}
 	
 
 	/**
-	 * TC02 - Nombre (Numero car·cteres < 4)
+	 * TC02 - Nombre (Numero car√°cteres < 4)
 	 */
 	@Test
 	void testSetNombreJugador2() {
 		Jugador jugador = new Jugador();
 		String nombre= "Pe";
 		jugador.setNombreJugador(nombre);
-		assertEquals(nombre,jugador.getNombreJugador(),"Car·cteres introducidos insuficientes");
+		assertEquals(nombre,jugador.getNombreJugador(),"Car√°cteres introducidos insuficientes");
 		
 		
 	}
 
 	/**
-	 * TC03 - Nombre (Numero car·cteres minimo)
+	 * TC03 - Nombre (Numero car√°cteres minimo)
 	 */
 	@Test
 	void testSetNombreJugador3() {
@@ -52,27 +52,27 @@ class JugadorTest {
 	}
 	
 	/**
-	 * TC04 - Nombre (Numero car·cteres maximo)
+	 * TC04 - Nombre (Numero car√°cteres maximo)
 	 */
 	@Test
 	void testSetNombreJugador4() {
 		Jugador jugador = new Jugador();
 		String nombre= "PepePepePepePepePepe";
 		jugador.setNombreJugador(nombre);
-		assertEquals(nombre, jugador.getNombreJugador(), "Car·cteres introducidos insuficientes");
+		assertEquals(nombre, jugador.getNombreJugador(), "Car√°cteres introducidos insuficientes");
 		
 
 	}
 	
 	/**
-	 * TC05 - Nombre (Numero car·cteres >20)
+	 * TC05 - Nombre (Numero car√°cteres >20)
 	 */
 	@Test
 	void testSetNombreJugador5() {
 		Jugador jugador = new Jugador();
 		String nombre= "PepePepePepePepePepeP";
 		jugador.setNombreJugador(nombre);
-		assertEquals(nombre, jugador.getNombreJugador(), "Excedido n˙mero car·cteres permitido");
+		assertEquals(nombre, jugador.getNombreJugador(), "Excedido n√∫mero car√°cteres permitido");
 		
 		
 	}
@@ -85,7 +85,7 @@ class JugadorTest {
 		Jugador jugador = new Jugador();
 		String nombre= "Pepe34";
 		jugador.setNombreJugador(nombre);
-		assertEquals(nombre, jugador.getNombreJugador(), "Car·cteres n˙mericos no permitidos");
+		assertEquals(nombre, jugador.getNombreJugador(), "Car√°cteres n√∫mericos no permitidos");
 		
 		
 	}
@@ -109,7 +109,7 @@ class JugadorTest {
 		Jugador jugador = new Jugador();
 		int edad= 18;
 		jugador.setEdad(edad);
-		assertEquals(edad, jugador.getEdad(), "Car·cter no v·lido");
+		assertEquals(edad, jugador.getEdad(), "Car√°cter no v√°lido");
 		
 	}
 	
@@ -121,7 +121,7 @@ class JugadorTest {
 		Jugador jugador = new Jugador();
 		int edad= -10;
 		jugador.setEdad(edad);
-		assertEquals(edad, jugador.getEdad(), "N˙meros negativos no permitidos");
+		assertEquals(edad, jugador.getEdad(), "N√∫meros negativos no permitidos");
 		
 	}
 	/**
@@ -158,7 +158,7 @@ class JugadorTest {
 	}
 
 	/**
-	 * TC13 - Idioma (EspaÒol). Mediante esta prueba, introduciremos un idioma valido. El test debera ser valido
+	 * TC13 - Idioma (Espa√±ol). Mediante esta prueba, introduciremos un idioma valido. El test debera ser valido
 	 */
 
 	@Test
@@ -166,7 +166,7 @@ class JugadorTest {
 		
 		 Jugador jug= new Jugador();
 		 
-		 String idioma= "EspaÒol";
+		 String idioma= "Espa√±ol";
 		 jug.setIdioma(idioma);
 		 assertEquals(idioma, jug.getIdioma());
 	}
@@ -180,13 +180,13 @@ class JugadorTest {
 		
 		 Jugador jug= new Jugador();
 		 
-		 String idioma= "Alem·n";
+		 String idioma= "Alem√°n";
 		 jug.setIdioma(idioma);
 		 assertEquals(idioma, jug.getIdioma());
 	}
 	
 	/**
-	 * TC15 - Idioma (Franc√©s). Mediante esta prueba, introduciremos un idioma valido. El test debera ser v√°lido
+	 * TC15 - Idioma (Franc√É¬©s). Mediante esta prueba, introduciremos un idioma valido. El test debera ser v√É¬°lido
 	 */
 
 	@Test
@@ -214,7 +214,7 @@ class JugadorTest {
 	}
 
 	/**
-	 * TC17 - Idioma (EspaÒol). Mediante esta prueba, introduciremos un idioma distinto de los disponibles. El test debera ser no valido
+	 * TC17 - Idioma (Espa√±ol). Mediante esta prueba, introduciremos un idioma distinto de los disponibles. El test debera ser no valido
 	 */
 
 	@Test
@@ -222,8 +222,229 @@ class JugadorTest {
 		
 		 Jugador jug= new Jugador();
 		 
-		 String idioma= "EspaÒol";
+		 String idioma= "Espa√±ol";
 		 jug.setIdioma(idioma);
 		 assertEquals(idioma, jug.getIdioma(),"El idioma introducido no es correcto.");
 	}
+	
+	//----------------------------Story 5 TESTS Aragon-------------------------------------------
+	
+		@Test
+    //T00- Todo null. OUTPUT-->Error
+    void tipoJugadorAllNull() {
+
+       Jugador jugador = new Jugador();
+       int edad=-1;
+       String nombre=null;
+       String idioma=null;
+       
+       jugador.setNombreJugador(nombre);
+       jugador.setIdioma(idioma);
+       jugador.setEdad(edad);
+       
+       String categoria=jugador.tipoJugador(edad);
+       
+       assertEquals(null, categoria);
+       assertNull(jugador.getNombreJugador());
+       assertNull(jugador.getIdioma());
+     }
+	
+	@Test
+    //T01- Edad null. OUTPUT-->Error
+    void tipoJugadorEdadNull() {
+
+       Jugador jugador = new Jugador();
+       int edad=-1;
+       String nombre="Pepe";
+       String idioma="Espa√±ol";
+       
+       jugador.setNombreJugador(nombre);
+       jugador.setIdioma(idioma);
+       jugador.setEdad(edad);
+       
+       String categoria=jugador.tipoJugador(edad);
+       
+       assertEquals(null, categoria);
+       assertNotNull(jugador.getNombreJugador());
+       assertNotNull(jugador.getIdioma());
+     }
+	
+	@Test
+    //T-02 Nombre Null
+    void tipoJugadorNombreNull() {
+
+        Jugador jugador = new Jugador();
+        int edad=19;
+        String nombre=null;
+        String idioma="Espa√±ol";
+        
+        jugador.setNombreJugador(nombre);
+        jugador.setIdioma(idioma);
+        jugador.setEdad(edad);
+        
+        String categoria=jugador.tipoJugador(edad);
+        
+        assertNotNull(categoria);
+        assertNull(jugador.getNombreJugador());
+        assertNotNull(jugador.getIdioma());
+      }
+	
+	@Test
+    //T-03 Idioma Null
+    void tipoJugadorIdiomaNull() {
+
+        Jugador jug = new Jugador();
+        int edad=19;
+        String nombre="Pepe";
+        String idioma=null;
+        
+        jug.setNombreJugador(nombre);
+        jug.setIdioma(idioma);
+        jug.setEdad(edad);
+        
+        String categoria=jug.tipoJugador(edad);
+        
+        assertNotNull(categoria);
+        assertNotNull(jug.getNombreJugador());
+        assertNull(jug.getIdioma());
+      }
+	
+	@Test
+    //T04- Edad fuera de Rango. OUTPUT-->Error
+    void tipoJugadorFueraRango() {
+
+        Jugador jugador = new Jugador();
+        int edad=8;
+        String nombre="Pepe";
+        String idioma="Espa√±ol";
+        
+        jugador.setNombreJugador(nombre);
+        jugador.setIdioma(idioma);
+        jugador.setEdad(edad);
+        
+        String categoria=jugador.tipoJugador(edad);
+        
+        assertEquals(null, categoria);
+        assertNotNull(jugador.getNombreJugador());
+        assertNotNull(jugador.getIdioma());
+      }
+	
+	@Test
+    //T05- Edad Junior Minimo Valor. OUTPUT-->OKEY
+    void tipoJugadorJuniorMin() {
+
+        Jugador jugador = new Jugador();
+        int edad=18;
+        String nombre="Pepe";
+        String idioma="Espa√±ol";
+        
+        jugador.setNombreJugador(nombre);
+        jugador.setIdioma(idioma);
+        jugador.setEdad(edad);
+        
+        String categoria=jugador.tipoJugador(edad);
+        
+        assertEquals("Junior", categoria);
+        assertNotNull(jugador.getNombreJugador());
+        assertNotNull(jugador.getIdioma());
+      }
+	
+	@Test
+    //T06- Edad Junior Minimo Valor. OUTPUT-->OKEY
+    void tipoJugadorJuniorMax() {
+
+        Jugador jugador = new Jugador();
+        int edad=24;
+        String nombre="Pepe";
+        String idioma="Espa√±ol";
+        
+        jugador.setNombreJugador(nombre);
+        jugador.setIdioma(idioma);
+        jugador.setEdad(edad);
+        
+        String categoria=jugador.tipoJugador(edad);
+        
+        assertEquals("Junior", categoria);
+        assertNotNull(jugador.getNombreJugador());
+        assertNotNull(jugador.getIdioma());
+      }
+	
+	@Test
+    //T07- Edad Senior. OUTPUT-->OKEY
+    void tipoJugadorSeniorMin() {
+
+        Jugador jugador = new Jugador();
+        int edad=25;
+        String nombre="Pepe";
+        String idioma="Espa√±ol";
+        
+        jugador.setNombreJugador(nombre);
+        jugador.setIdioma(idioma);
+        jugador.setEdad(edad);
+        
+        String categoria=jugador.tipoJugador(edad);
+        
+        assertEquals("Senior", categoria);
+        assertNotNull(jugador.getNombreJugador());
+        assertNotNull(jugador.getIdioma());
+      }
+	
+	@Test
+    //T08- Edad Senior. OUTPUT-->OKEY
+    void tipoJugadorSeniorMax() {
+
+        Jugador jugador = new Jugador();
+        int edad=35;
+        String nombre="Pepe";
+        String idioma="Espa√±ol";
+        
+        jugador.setNombreJugador(nombre);
+        jugador.setIdioma(idioma);
+        jugador.setEdad(edad);
+        
+        assertEquals("Senior", jugador.tipoJugador(edad));
+        assertNotNull(jugador.getNombreJugador());
+        assertNotNull(jugador.getIdioma());
+      }
+	
+	@Test
+    //T09- Edad Master min. OUTPUT-->OKEY
+    void tipoJugadorMasterMin() {
+
+        Jugador jug = new Jugador();
+        int edad=36;
+        String nombre="Pepe";
+        String idioma="Espa√±ol";
+        
+        jug.setNombreJugador(nombre);
+        jug.setIdioma(idioma);
+        jug.setEdad(edad);
+        
+        String categoria=jug.tipoJugador(edad);
+        
+        assertEquals("Master", categoria);
+        assertNotNull(jug.getNombreJugador());
+        assertNotNull(jug.getIdioma());
+      }
+	
+	@Test
+    //T10- Edad Master Max. OUTPUT-->OKEY
+    void tipoJugadorMasterAlto() {
+
+        Jugador jug = new Jugador();
+        int edad=70;
+        String nombre="Pepe";
+        String idioma="Espa√±ol";
+        
+        jug.setNombreJugador(nombre);
+        jug.setIdioma(idioma);
+        jug.setEdad(edad);
+        
+        String categoria=jug.tipoJugador(edad);
+        String buena="Master";
+        
+        assertEquals(buena, categoria);
+        assertNotNull(jug.getNombreJugador());
+        assertNotNull(jug.getIdioma());
+      }
 }
