f866be3381808fb093f3b0f37769a68f851b1893
Mr-BlueA <arturo.s238739@cesurformacion.com>
2022-02-06 17:53:33 +0100

    Asignar equipo

--- AsignarEquipo.java
+++ AsignarEquipo.java
@@ -1,3 +1,4 @@
+import java.util.Objects;
 
 public class AsignarEquipo {
       private Equipo equipo;
@@ -9,13 +10,22 @@ public class AsignarEquipo {
 //    	  this.setJugador(jugador);
     	  
       }
-      
       public void setEquipo(Equipo equipo) {
-  		//code here
+  		if (equipo.getNombreEquipo().length()>=4 && equipo.getNombreEquipo().length()<=20 && equipo.getNombreEquipo()!=null);{
+                if (equipo.getRanking()>=0 && equipo.getRanking()<=10) {
+                      this.equipo=equipo;
+                }
+            }
       }
       
       public void setJugador(Jugador jugador) {
-  		//code here
+  		if (jugador.getNombreJugador().length()>=4 && jugador.getNombreJugador().length()<=20 && jugador.getNombreJugador()!=null) {
+            if (jugador.getEdad()>0) {
+                if (Objects.equals(jugador.getIdioma(), "Frances") || Objects.equals(jugador.getIdioma(), "Español") || Objects.equals(jugador.getIdioma(), "Aleman")) {
+                    this.jugador=jugador;
+                }
+            }
+        }
       }
       
       
