4bda3ce399f80136a2aba0528c1c2e3bb731cff4
Mr-BlueA <arturo.s238739@cesurformacion.com>
2022-02-13 18:10:57 +0100

    Equipo

--- src/Equipo.java
+++ src/Equipo.java
@@ -1,14 +1,27 @@
+
+/**
+ * Esta clase sirve para crear un equipo
+ * @author: Arturo
+ * @version: 13/02/2022
+ */
+
 public class Equipo {
+//	Campos de la clase
 	private String nombreEquipo;
 	private int ranking;
-	
-	
+
+/**
+* Constructor del equipo
+*/
 	public Equipo () {
-//		this.setNombreEquipo(nombreEquipo);
-//		this.setRanking(ranking);
-	}
-	
-	
+		this.setNombreEquipo(nombreEquipo);
+		this.setRanking(ranking);
+	} // Cierre del constructor
+
+/**
+* Metodo que guarda el nombre del equipo
+* @param nombreEquipo El parámetro es el String con el nombre del equipo
+*/
 	public void setNombreEquipo(String nombreEquipo) {
 		boolean cumple=true;
 		if(nombreEquipo!=null && nombreEquipo.length()>=4&&nombreEquipo.length()<=20){
@@ -35,6 +48,11 @@ public class Equipo {
 		
 	}
 	}
+
+/**
+* Metodo que guarda el el ranking del equipo
+* @param ranking El parámetro es el int con el cual se decide el ranking
+*/
 	public void setRanking(int ranking) {
 
 		if(ranking >=0 && ranking <=10) {
@@ -44,15 +62,25 @@ public class Equipo {
 		
 
 	}
-	
+
+/**
+* Método que devuelve el nombre del equipo
+* @return El nombre del equipo
+*/
 	public String getNombreEquipo() {
 		return nombreEquipo;
 	}
+/**
+* Método que devuelve el ranking guardado
+* @return El ranking guardado
+*/
 	public int getRanking() {
 		return ranking;
 	}
 
-
+/**
+* Método que asigna la categoria al equipo segun el ranking de este
+*/
 	public String categoriaEquipo() {
 	String categoria = "";
 	   if(nombreEquipo!=null && ranking!=-1) {
