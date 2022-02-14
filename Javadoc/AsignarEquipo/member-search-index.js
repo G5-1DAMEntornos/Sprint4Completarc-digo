c45609b00f102252269b308f63beb5fcf2a702f4
Erkus00 <91884756+Erkus00@users.noreply.github.com>
2022-02-14 19:22:53 +0100

    A

--- Javadoc/AsignarEquipo/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"<Unnamed>","c":"AsignarEquipo","l":"AsignarEquipo()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"AsignarEquipo","l":"getEquipo()"},{"p":"<Unnamed>","c":"AsignarEquipo","l":"getJugador()"},{"p":"<Unnamed>","c":"AsignarEquipo","l":"setEquipo(Equipo)"},{"p":"<Unnamed>","c":"AsignarEquipo","l":"setJugador(Jugador)"}];updateSearchResults();
